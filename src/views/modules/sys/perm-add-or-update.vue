f79ca68a0e3b77e15953ffdf9cd01841459794de
AlexReagan <1569813053@qq.com>
2021-11-18 15:15:20 +0800

    [feature]: 增加权限管理功能

---
+++ src/views/modules/sys/perm-add-or-update.vue
@@ -0,0 +1,99 @@
+<template>
+  <el-dialog
+    :title="!dataForm.id ? '新增' : '修改'"
+    :close-on-click-modal="false"
+    :visible.sync="visible">
+    <el-form :model="dataForm" :rules="dataRule" ref="dataForm" @keyup.enter.native="dataFormSubmit()" label-width="80px">
+      <el-form-item label="名称" prop="name">
+        <el-input v-model="dataForm.name" placeholder="请用按照:分割标识权限层级"></el-input>
+      </el-form-item>
+      <el-form-item label="中文名称" prop="cnName">
+        <el-input v-model="dataForm.cnName" placeholder="权限的中文名称"></el-input>
+      </el-form-item>
+      <el-form-item label="备注" prop="remark">
+        <el-input v-model="dataForm.remark" placeholder="备注"></el-input>
+      </el-form-item>
+    </el-form>
+    <span slot="footer" class="dialog-footer">
+      <el-button @click="visible = false">取消</el-button>
+      <el-button type="primary" @click="dataFormSubmit()">确定</el-button>
+    </span>
+  </el-dialog>
+</template>
+
+<script>
+  // import { treeDataTranslate } from '@/utils'
+  export default {
+    data () {
+      return {
+        visible: false,
+        dataForm: {
+          id: 0,
+          name: '',
+          cnName: '',
+          remark: ''
+        },
+        dataRule: {
+          name: [
+            { required: true, message: '权限名称不能为空', trigger: 'blur' }
+          ],
+          cnName: [
+            { required: true, message: '中文名称不能为空', trigger: 'blur' }
+          ]
+        }
+      }
+    },
+    methods: {
+      init (id) {
+        this.visible = true
+        this.dataForm.id = id || 0
+        if (this.dataForm.id) {
+          this.$http({
+            url: this.$http.adornUrl(`/api/v1/perm/info`),
+            method: 'get',
+            params: this.$http.adornParams({
+              'id': this.dataForm.id
+            })
+          }).then(({data}) => {
+            if (!data.error) {
+              this.dataForm.name = data.perm.name
+              this.dataForm.cnName = data.perm.cnName
+              this.dataForm.remark = data.perm.remark
+            }
+          })
+        }
+      },
+      // 表单提交
+      dataFormSubmit () {
+        this.$refs['dataForm'].validate((valid) => {
+          if (valid) {
+            this.$http({
+              url: this.$http.adornUrl(`/api/v1/perm/${!this.dataForm.id ? 'create' : 'update'}`),
+              method: !this.dataForm.id ? 'post' : 'put',
+              data: this.$http.adornData({
+                'id': this.dataForm.id || undefined,
+                'name': this.dataForm.name,
+                'cnName': this.dataForm.cnName,
+                'remark': this.dataForm.remark
+              })
+            }).then(({data}) => {
+              if (!data.error) {
+                this.$message({
+                  message: '操作成功',
+                  type: 'success',
+                  duration: 1500,
+                  onClose: () => {
+                    this.visible = false
+                    this.$emit('refreshDataList')
+                  }
+                })
+              } else {
+                this.$message.error(data.msg)
+              }
+            })
+          }
+        })
+      }
+    }
+  }
+</script>

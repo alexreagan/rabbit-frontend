e710ed76024944311037336000edbff60735bf8c
dh <dh@mail.com>
2021-12-20 11:23:56 +0800

    feat: 引入g6-editor

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
   root: true,
   parser: 'babel-eslint',
   parserOptions: {
-    sourceType: 'module'
+    sourceType: 'module',
   },
   env: {
     browser: true,
@@ -12,14 +12,16 @@ module.exports = {
   // https://github.com/standard/standard/blob/master/docs/RULES-en.md
   extends: 'standard',
   // required to lint *.vue files
-  plugins: [
-    'html'
-  ],
+  plugins: ['html'],
   // add your custom rules here
   rules: {
+    quotes: [2, 'single'], //单引号
+    semi: [2, 'never'], // 不使用分号
+    'space-before-function-paren': [2, 'never'], // 函数前面加上空格
+    'one-var': 0,
     // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  }
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  },
 }

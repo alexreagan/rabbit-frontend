f2ef5086619d3bf09838763fd0c0bc7d59b705bc
Xin Liu <lonelycloudy@gmail.com>
2022-02-18 13:48:51 +0800

    [feature]: 已办

--- src/views/common/home.vue
+++ src/views/common/home.vue
@@ -17,7 +17,7 @@
             <el-table-column prop="PCS_AVY_NM" label="当前环节"> </el-table-column>
             <el-table-column prop="WF_EXTR_NM" label="发送人"> </el-table-column>
             <el-table-column prop="TODO_START_TM" label="办理时间"> </el-table-column>
-            <el-table-column label="操作"> 
+            <el-table-column label="操作">
                 <template slot-scope="scope">
                     <el-button type="text" size="small" @click="procTodoHandle(scope.row.TEMPLATE_ID, scope.row.PROCESS_INST_ID, scope.row.TASK_ID, scope.row.TODO_SN, scope.row.PRJ_SN)">办理</el-button>
                 </template>
@@ -48,6 +48,39 @@
         </ul>
       </div>
     </el-card>
+
+    <el-card class="card-hasDone">
+      <div slot="header" class="clearfix">
+        <span>已办事项</span>
+      </div>
+      <div class="proc-hasDone">
+        <template>
+          <el-table :data="hasDone" stripe border style="width: 100%">
+            <el-table-column prop="PRJ_BELONG_TYPE" label="流程分类"></el-table-column>
+            <el-table-column prop="PRJ_TYPE" label="流程名称"></el-table-column>
+            <el-table-column prop="PROCESS_INST_ID" label="编号"> </el-table-column>
+            <el-table-column prop="PRJ_NM" label="标题"> </el-table-column>
+            <el-table-column prop="PCS_AVY_NM" label="当前环节"> </el-table-column>
+            <el-table-column prop="WF_EXTR_NM" label="发送人"> </el-table-column>
+            <el-table-column prop="TODO_START_TM" label="办理时间"> </el-table-column>
+            <el-table-column label="操作">
+              <template slot-scope="scope">
+                <el-button type="text" size="small" @click="procHasDoneHandle(scope.row.TEMPLATE_ID, scope.row.PROCESS_INST_ID, scope.row.TASK_ID, scope.row.TODO_SN, scope.row.PRJ_SN)">办理</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+          <el-pagination
+            @size-change="sizeChangeHandle"
+            @current-change="currentChangeHandle"
+            :current-page="pageIndex"
+            :page-sizes="[10, 20, 50, 100]"
+            :page-size="pageSize"
+            :total="totalPage"
+            layout="total, sizes, prev, pager, next, jumper">
+          </el-pagination>
+        </template>
+      </div>
+    </el-card>
   </div>
 </template>
 
@@ -57,6 +90,7 @@ export default {
     return {
       notices: [],
       todos: [],
+      hasDone: [],
       pageIndex: 1,
       pageSize: 10,
       totalPage: 0,
@@ -106,6 +140,34 @@ export default {
           this.pageSize = 10
           this.totalPage = 0
         })
+        // 已办消息
+        this.$http({
+          url: this.$http.adornUrl(`/api/v1/wfe/hasDone`),
+          method: 'post',
+          params: this.$http.adornParams({
+            page: this.pageIndex,
+            limit: this.pageSize
+          })
+        }).then(({data}) => {
+          this.dataListLoading = false
+          let hasDone = []
+          if (data.TX_BODY.ENTITY.HASDONE_INFO) {
+            hasDone = data.TX_BODY.ENTITY.HASDONE_INFO
+          }
+          let totalPage = 0
+          if (data.TX_BODY.COMMON.COMB.TOTAL_PAGE) {
+            totalPage = data.TX_BODY.COMMON.COMB.TOTAL_PAGE
+          }
+          this.hasDone = hasDone
+          this.totalPage = totalPage
+        }).catch(error => {
+          this.dataListLoading = false
+          this.$message.error(error.messag)
+          this.hasDone = []
+          this.pageIndex = 1
+          this.pageSize = 10
+          this.totalPage = 0
+        })
       })
     },
     // 每页数
@@ -122,6 +184,11 @@ export default {
     // 处理待办
     procTodoHandle (templateID, processInstID, taskID, todoID, prjSN) {
       this.$router.push({name: 'pub-flow', query: {'templateID': templateID, 'processInstID': processInstID, 'taskID': taskID, 'todoID': todoID, 'prjSN': prjSN}})
+      console.log(processInstID)
+    },
+    // 处理已办
+    procHasDoneHandle (templateID, processInstID, taskID, todoID, prjSN) {
+      // this.$router.push({name: 'pub-flow', query: {'templateID': templateID, 'processInstID': processInstID, 'taskID': taskID, 'todoID': todoID, 'prjSN': prjSN}})
       console.log(processInstID)
     },
     clickNotice(id) {
@@ -179,5 +246,11 @@ a:focus, a:hover {
 .notice-item ul li{
     list-style-type: none;
     margin-bottom: 2px;
+}
+.card-hasDone {
+  float: left;
+  margin-top: 10px;
+  margin-right: 10px;
+  width: 79%;
 }
 </style>
